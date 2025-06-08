d818359c6d1ea8b23456837922a571eb8b4e3add
Joshtri <stuffofyos1516@gmail.com>
2025-06-09 00:56:29 +0700

    refactor: enhance Layout component structure and styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from './partials/Header';
-import FooterCustom from './partials/Footer';
-// import Footer from './partials/Footer';
+import Header from './partials/Header'
+import FooterCustom from './partials/Footer'
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-      <main className="flex-grow" style={{ padding: '1rem', maxWidth: '1200px', margin: '0 auto' }}>
-        {children}
+      <main className="flex-grow w-full">
+        <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+          {children}
+        </div>
       </main>
-      <FooterCustom/>
+      <FooterCustom />
     </div>
-  );
+  )
 }
