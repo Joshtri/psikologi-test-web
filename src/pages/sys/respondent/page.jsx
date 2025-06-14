f125f71069152b9000e5ad38615500b0c71ddf4e
Joshtri <stuffofyos1516@gmail.com>
2025-06-14 10:19:04 +0700

    feat: add RespondentPage component to display and manage respondent data; implement loading state and error handling

---
+++ src/pages/sys/respondent/page.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import ActionButtons from "@/components/ui/ActionButtons";
+import PageBreadcrumb from "@/components/ui/PageBreadcrumb";
+import Table from "@/components/ui/Table";
+import { getRespondents } from "@/services/respondent.service"; // pastikan path sesuai
+import { useNavigate } from "react-router-dom";
+
+export default function RespondentPage() {
+  const [respondents, setRespondents] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  const columns = [
+    { label: "No", key: "index", render: (_row, i) => i + 1 },
+    { label: "Nama", key: "name" },
+    { label: "Usia", key: "age" },
+    { label: "Jenis Kelamin", key: "gender" },
+    { label: "Pendidikan", key: "educationLevel" },
+    { label: "No. HP", key: "phoneNumber" },
+    {
+      label: "Aksi",
+      key: "actions",
+      render: (row) => (
+        <ActionButtons
+          onDetail={() => navigate(`/sys/respondents/${row.id}`)}
+          onEdit={() => console.log("Edit:", row)}
+          onDelete={() => console.log("Hapus:", row)}
+        />
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    const fetchRespondents = async () => {
+      try {
+        const data = await getRespondents();
+        setRespondents(data);
+      } catch (error) {
+        console.error("Gagal memuat data responden:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRespondents();
+  }, []);
+
+  return (
+    <div className="mx-auto p-6">
+      <PageBreadcrumb items={[{ label: "Data Responden" }]} />
+      <h1 className="text-2xl font-bold mb-6">Daftar Responden</h1>
+
+      {loading ? (
+        <p>Memuat data...</p>
+      ) : (
+        <Table columns={columns} data={respondents} />
+      )}
+    </div>
+  );
+}
